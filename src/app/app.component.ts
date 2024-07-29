07d1c3f7220eb4eca20f78ee9ef1d7243c888cbb
shadyyd <yd.radwan@gmail.com>
2024-07-29 23:30:53 +0300

    finshed task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeroComponent } from './hero/hero.component';
+import { BioComponent } from './bio/bio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeroComponent,
+    BioComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Portfolio';
